2fe39fe0589ff1a3664e0a9188d9b6abe6bda511
Marta <martitaoun@gmail.com>
2020-05-16 12:38:43 +0200

    Hemos agregado un programa que lee un numero

--- src/ejemplo/Prueba.java
+++ src/ejemplo/Prueba.java
@@ -1,11 +1,23 @@
+/*Programa que lea un número entero N de 5 cifras y muestre sus cifras
+desde el final.*/
+
 package ejemplo;
+
+import java.util.Scanner;
 
 public class Prueba {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
-
+		Scanner sc=new Scanner (System.in);
+		
+		System.out.println("Introduce un número de 5 cifras");
+		String numero=sc.nextLine();
+		
+		System.out.println(numero.length()!=5? "Valor introducido no válido": 
+			numero.substring (4,5)+ numero.substring(3,4)+ numero.substring(2,3)+
+			numero.substring(1,2)+numero.substring(0,1));
+		
+		sc.close();
 	}
-
 }
